ea5037322096eaf2aa1bb424194ad879d44278c8
notlober <selahattinbakidamar1@gmail.com>
2024-04-29 19:45:41 +0300

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+git clone https://github.com/facebookresearch/cc_net.git
+
+cd cc_net
+
+mkdir -p data
+
+sudo apt-get update
+
+sudo apt-get install libeigen3-dev -y
+
+sudo apt-get install libboost-all-dev -y
+
+make install
+
+make lang=tr dl_lm
+
+pip install cc_net[getpy]
